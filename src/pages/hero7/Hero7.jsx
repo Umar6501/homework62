4d431c992cadcd6cc0037772fd69a8eabaae9111
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-09-21 10:21:45 +0500

    hom62

--- src/pages/hero7/Hero7.jsx
+++ src/pages/hero7/Hero7.jsx
@@ -6,45 +6,49 @@ function Hero7() {
     <section className="hero7">
       <div className="container-7">
         <div className="hero7-content">
-          <div className="h1-content">
-            <h1 className="h1">Quality cleaning for your home</h1>
-            <p className="h2">
-              Lorem ipsum dolor sit amet cteturdo adipiscing elit, sed do
-              eiusmo.
-            </p>
-            <div className="jj">
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
+          <div className="one">
+            <div className="h1-content">
+              <h1 className="h1">Quality cleaning for your home</h1>
+              <p className="h2">
+                Lorem ipsum dolor sit amet cteturdo adipiscing elit, sed do
+                eiusmo.
+              </p>
+              <div className="jj">
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+            <div className="h2-content">
+              <h1 className="h1">Contact us</h1>
+              <p className="h2">
+                1827 Nickel Road, Los Angeles, CA, 90017, United States
+              </p>
+              <p className="h3">(414) 567 - 2109 </p>
+              <p className="h4">contact@cleaning.com</p>
             </div>
           </div>
-          <div className="h2-content">
-            <h1 className="h1">Contact us</h1>
-            <p className="h2">
-              1827 Nickel Road, Los Angeles, CA, 90017, United States
-            </p>
-            <p className="h3">(414) 567 - 2109 </p>
-            <p className="h4">contact@cleaning.com</p>
-          </div>
-          <div className="h3-content">
-            <h1 className="h1">Hours</h1>
-            <h4 className="h2">Monday to Friday</h4>
-            <p className="h3">6:00 AM - 9:00 PM</p>
-            <h4 className="h4">Saturday & Sunday</h4>
-            <p className="h5">8:00 AM - 8:00 PM</p>
-          </div>
-          <div className="h4-content">
-            <h1 className="h1">Get a free estimate</h1>
-            <h4 className="h2">(414) 567 - 2109</h4>
-            <p className="h3">
-              Lorem ipsum dolor sit amet ectetur adipiscing elit, sed do
-              eiusmod.
-            </p>
-            <a href="#" className="h4">
-              Request a free quote
-            </a>
+          <div className="two">
+            <div className="h3-content">
+              <h1 className="h1">Hours</h1>
+              <h4 className="h2">Monday to Friday</h4>
+              <p className="h3">6:00 AM - 9:00 PM</p>
+              <h4 className="h4">Saturday & Sunday</h4>
+              <p className="h5">8:00 AM - 8:00 PM</p>
+            </div>
+            <div className="h4-content">
+              <h1 className="h1">Get a free estimate</h1>
+              <h4 className="h2">(414) 567 - 2109</h4>
+              <p className="h3">
+                Lorem ipsum dolor sit amet ectetur adipiscing elit, sed do
+                eiusmod.
+              </p>
+              <a href="#" className="h4">
+                Request a free quote
+              </a>
+            </div>
           </div>
         </div>
       </div>
